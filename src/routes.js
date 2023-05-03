c541490ca18f0624166484b19c953f83b05d5e25
indexhtml821 <isaacopine@gmail.com>
2023-05-03 13:58:32 -0600

    se añade la ruta de welcome correctamente

--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,9 @@
-import React from 'react'
-import { Text, Reply } from '@botonic/react'
-import welcome from './actions/welcome'
+import Welcome from './actions/welcome'
 
 export const routes = [
   {
-    path: 'initial',
-    text: /hi/i,
-    action: welcome,
-
-    /* () => (
-      <>
-        <Text>Hello! Nice to meet you ;)</Text>
-        <Text>
-          How can I help you?
-          <Reply payload="search">Search product</Reply>
-          <Reply payload="track">Track my order</Reply>
-        </Text>
-      </>
-    )*/
-  }
+    path: 'welcome',
+    text: /hi|hello/,
+    action: Welcome,
+  },
 ]
