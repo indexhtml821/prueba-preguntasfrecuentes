9e42d41d19e024cab15e4ae01cb2adc9e2a76d2f
indexhtml821 <isaacopine@gmail.com>
2023-05-17 10:50:43 -0600

    se añade un carrusel con opciones de inscripción a cursos

--- src/actions/chosen-inscripcion.js
+++ src/actions/chosen-inscripcion.js
@@ -1,13 +1,56 @@
 import React from 'react'
-import { Text,customMessage } from '@botonic/react'
-
-function MyForm(){
-    const{submit} = useSubmit()
-}
-
+import {
+  Text,
+  Carousel,
+  Element,
+  Pic,
+  Button,
+  Title,
+  Subtitle,
+} from '@botonic/react'
+//import { Link } from 'react-router-dom';
 
 export default class extends React.Component {
   render() {
-    return <Text>Pasta is always a good choice!</Text>
+
+    const manejarClick = ()=>{
+      event.preventDefault();
+      window
+    };
+    const courses = [
+      {
+        nombre: 'Comunicación Coherente, Asertiva y Empática en la Docencia',
+        dif: '🌟 🌟 🌟 🌟',
+        link:'https://metics.ucr.ac.cr/es/component/sppagebuilder/32-comunicacion-coherente-asertiva-y-empatica-en-la-docencia',
+        foto:'https://metics.ucr.ac.cr/images/Documentos/Marco_Competencias/Caretas_Modulos/careta_comunicacion_coherente.png',
+      },
+      {
+        nombre: 'Construcción de Ciudadanía Digital desde la Docencia',
+        dif: '🌟 🌟 🌟 🌟 🌟',
+        link:'https://metics.ucr.ac.cr/es/component/sppagebuilder/34-construccion-de-ciudadania-digital-desde-la-docencia',
+        foto:'https://metics.ucr.ac.cr/images/Documentos/Marco_Competencias/Caretas_Modulos/careta_construccion_ciudadania.png',
+      },
+      {
+        nombre: 'Diseño Visual Aplicado a Entornos Virtuales',
+        dif: '🌟 🌟',
+        link:'https://metics.ucr.ac.cr/es/component/sppagebuilder/39-diseno-visual-aplicado-a-entornos-virtuales',
+        foto: 'https://metics.ucr.ac.cr/images/Documentos/Marco_Competencias/Caretas_Modulos/careta_diseo_visual.png',
+      },
+    ]
+    return (
+      <>
+        <Text>Seleccione un curso:</Text>
+        <Carousel>
+          {courses.map((e, i) => (
+            <Element key={e.nombre}>
+              <Pic src={e.foto} />
+              <Title>{e.nombre}</Title>
+              <Subtitle>{e.dif}</Subtitle>
+              <Button url={e.link} >{e.nombre}</Button>
+            </Element>
+          ))}
+        </Carousel>
+      </>
+    )
   }
 }
