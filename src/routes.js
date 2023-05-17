ed60a6feb687915e59141a47d47f1ce1c7a5cce5
indexhtml821 <isaacopine@gmail.com>
2023-05-17 10:49:43 -0600

    bienvenida y subrutas opcionales

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,23 @@
 import Welcome from './actions/welcome'
+import ChosenContacto from './actions/chosen-contacto'
+import chosenInscripcion from './actions/chosen-inscripcion'
 
 export const routes = [
   {
     path: 'welcome',
     text: /hi|hello/,
     action: Welcome,
+    childRoutes: [
+      {
+        path: 'chosen-contacto',
+        payload: 'contacto',
+        action: ChosenContacto,
+      },
+      {
+        path: 'chosen-inscripcion',
+        payload: 'inscripcion',
+        action: chosenInscripcion,
+      },
+    ],
   },
 ]
